744891f19ee053f65142648b47ef4279dd49c464
madvirus <madvirus@madvirus.net>
2013-12-20 11:59:51 +0900

    <import> 태그 추가 및 잘못된 설정 삭제

---
+++ mvnprj/spring4-chap02/src/main/java/net/madvirus/spring4/chap02/MainByAll.java
@@ -0,0 +1,21 @@
+package net.madvirus.spring4.chap02;
+
+import net.madvirus.spring4.chap02.sensor.Monitor;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainByAll {
+
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx =
+				new GenericXmlApplicationContext("classpath:config-all.xml");
+		AuthenticationService authSvc =
+				ctx.getBean("authenticationService", AuthenticationService.class);
+		authSvc.authenticate("bkchoi", "1234");
+
+		Monitor monitor = ctx.getBean(Monitor.class);
+		System.out.println(monitor);
+		ctx.close();
+	}
+
+}

a963cb1d8d07768f8599a0f275a93652af3ad4a2
madvirus <madvirus@madvirus.net>
2013-12-24 17:49:32 +0900

    코드 수정

--- mvnprj/spring4-chap02/src/main/java/net/madvirus/spring4/chap02/erp/DefaultErpClientFactory.java
+++ mvnprj/spring4-chap02/src/main/java/net/madvirus/spring4/chap02/erp/DefaultErpClientFactory.java
@@ -1,18 +1,26 @@
 package net.madvirus.spring4.chap02.erp;
 
+import java.util.Properties;
+
 class DefaultErpClientFactory extends ErpClientFactory {
+
+	private String server;
+	
+	public DefaultErpClientFactory(Properties props) {
+		this.server = props.getProperty("server");
+	}
 
 	@Override
 	public ErpClient create() {
 		return new ErpClient() {
 			@Override
 			public void connect() {
-				System.out.println("연결함");
+				System.out.println("연결함:"+server);
 			}
 
 			@Override
 			public void close() {
-				System.out.println("연결 끊음");
+				System.out.println("연결 끊음:"+server);
 			}
 		};
 	}
--- mvnprj/spring4-chap02/src/main/java/net/madvirus/spring4/chap02/erp/ErpClientFactory.java
+++ mvnprj/spring4-chap02/src/main/java/net/madvirus/spring4/chap02/erp/ErpClientFactory.java
@@ -1,14 +1,20 @@
 package net.madvirus.spring4.chap02.erp;
 
-public abstract class ErpClientFactory {
-	private static ErpClientFactory factory = new DefaultErpClientFactory();
+import java.util.Properties;
 
+public abstract class ErpClientFactory {
 	public static ErpClientFactory instance() {
-		return factory;
+		Properties props = new Properties();
+		props.setProperty("server", "localhost");
+		return instance(props);
+	}
+
+	public static ErpClientFactory instance(Properties props) {
+		return new DefaultErpClientFactory(props);
 	}
 
 	protected ErpClientFactory() {
 	}
-	
+
 	public abstract ErpClient create();
 }

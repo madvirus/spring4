ac30c4a625c5b31d29d1cf9453c8739b82184d68
madvirus <madvirus@madvirus.net>
2014-01-04 20:38:49 +0900

    이름 변경

--- mvnprj/spring4-chap03/src/main/java/net/madvirus/spring4/chap03/ConfigForLifecycle.java
+++ mvnprj/spring4-chap03/src/main/java/net/madvirus/spring4/chap03/ConfigForLifecycle.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class ConfigForLifecycle {
 
 	@Bean
 	public MyBean myBean() {
--- mvnprj/spring4-chap03/src/main/java/net/madvirus/spring4/chap03/MainForLifecycle.java
+++ mvnprj/spring4-chap03/src/main/java/net/madvirus/spring4/chap03/MainForLifecycle.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class Main {
+public class MainForLifecycle {
 
 	public static void main(String[] args) {
 		useXml();
@@ -13,7 +13,7 @@ public class Main {
 
 	private static void useXml() {
 		System.out.println("===== XML Meta =====");
-		String configLocation = "classpath:applicationContext.xml";
+		String configLocation = "classpath:config-for-lifecycle.xml";
 		AbstractApplicationContext ctx =
 				new GenericXmlApplicationContext(configLocation);
 		ctx.close();
@@ -21,7 +21,7 @@ public class Main {
 
 	private static void useJava() {
 		System.out.println("===== Java Meta =====");
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ConfigForLifecycle.class);
 		ctx.close();
 
 	}

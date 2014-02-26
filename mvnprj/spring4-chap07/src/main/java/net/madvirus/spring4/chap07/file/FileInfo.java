9a56f1a184d3fc52728ecf11a0756908c97bbd55
madvirus <madvirus@madvirus.net>
2014-02-27 08:56:46 +0900

    사소한 수정

--- mvnprj/spring4-chap07/src/main/java/net/madvirus/spring4/chap07/file/FileInfo.java
+++ mvnprj/spring4-chap07/src/main/java/net/madvirus/spring4/chap07/file/FileInfo.java
@@ -2,4 +2,14 @@ package net.madvirus.spring4.chap07.file;
 
 public class FileInfo {
 
+	private String fileId;
+
+	public FileInfo(String fileId) {
+		this.fileId = fileId;
+	}
+
+	public String getFileId() {
+		return fileId;
+	}
+
 }
--- mvnprj/spring4-chap07/src/main/java/net/madvirus/spring4/chap07/file/NoFileInfoException.java
+++ mvnprj/spring4-chap07/src/main/java/net/madvirus/spring4/chap07/file/NoFileInfoException.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-@SuppressWarnings("serial")
 public class NoFileInfoException extends Exception {
+
+	private static final long serialVersionUID = 1L;
 
 }

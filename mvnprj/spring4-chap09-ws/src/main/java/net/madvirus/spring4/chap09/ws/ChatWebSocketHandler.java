c34e6b482b6ac3c1d3c56d3937e9f7ce60e5df95
madvirus <madvirus@madvirus.net>
2014-04-01 21:15:46 +0900

    포맷팅

--- mvnprj/spring4-chap09-ws/src/main/java/net/madvirus/spring4/chap09/ws/ChatWebSocketHandler.java
+++ mvnprj/spring4-chap09-ws/src/main/java/net/madvirus/spring4/chap09/ws/ChatWebSocketHandler.java
@@ -14,19 +14,22 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 	private Map<String, WebSocketSession> users = new HashMap<>();
 
 	@Override
-	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+	public void afterConnectionEstablished(
+			WebSocketSession session) throws Exception {
 		log(session.getId() + " 연결 됨");
 		users.put(session.getId(), session);
 	}
 
 	@Override
-	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+	public void afterConnectionClosed(
+			WebSocketSession session, CloseStatus status) throws Exception {
 		log(session.getId() + " 연결 종료됨");
 		users.remove(session.getId());
 	}
 
 	@Override
-	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+	protected void handleTextMessage(
+			WebSocketSession session, TextMessage message) throws Exception {
 		log(session.getId() + "로부터 메시지 수신: " + message.getPayload());
 		for (WebSocketSession s : users.values()) {
 			s.sendMessage(message);
@@ -35,7 +38,8 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 	}
 
 	@Override
-	public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+	public void handleTransportError(
+			WebSocketSession session, Throwable exception) throws Exception {
 		log(session.getId() + " 익셉션 발생: " + exception.getMessage());
 	}
 

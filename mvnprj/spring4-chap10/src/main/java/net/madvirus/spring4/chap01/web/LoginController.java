6d18c767a1ef44a347a03921ea96012658a81425
madvirus <madvirus@madvirus.net>
2014-04-02 16:11:07 +0900

    시작

---
+++ mvnprj/spring4-chap10/src/main/java/net/madvirus/spring4/chap01/web/LoginController.java
@@ -0,0 +1,8 @@
+package net.madvirus.spring4.chap01.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+}
---
+++ mvnprj/spring4-chap10/src/main/java/net/madvirus/spring4/chap01/webconf/SpringServletConfig.java
@@ -0,0 +1,20 @@
+package net.madvirus.spring4.chap01.webconf;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class SpringServletConfig implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+//	org.springframework.web.WebApplicationInitializer
+//	org.springframework.web.context.AbstractContextLoaderInitializer
+//	org.springframework.web.servlet.support.AbstractDispatcherServletInitializer
+//	org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer
+}

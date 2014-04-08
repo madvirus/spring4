6ba8aae2c18af3989e997f0dda022c213d85b16e
madvirus <madvirus@madvirus.net>
2014-04-08 09:22:59 +0900

    query

---
+++ mvnprj/spring4-chap11/src/ddl/mysql.ddl.sql
@@ -0,0 +1,12 @@
+create user 'spring4'@'localhost' identified by 'spring4';
+
+create database guestbook character set=utf8;
+
+grant all privileges on guestbook.* to 'spring4'@'localhost';
+
+create table guestmessage (
+	id int auto_increment primary key,
+	name varchar(100),
+	message text,
+	creationTime datetime
+) engine=InnoDB character set = utf8;

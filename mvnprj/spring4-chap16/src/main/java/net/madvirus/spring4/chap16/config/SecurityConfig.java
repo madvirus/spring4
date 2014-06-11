a6e04123ad7e22928d8819fd4a5c9e426937d065
madvirus <madvirus@madvirus.net>
2014-06-12 08:47:28 +0900

    working

--- mvnprj/spring4-chap16/src/main/java/net/madvirus/spring4/chap16/config/SecurityConfig.java
+++ mvnprj/spring4-chap16/src/main/java/net/madvirus/spring4/chap16/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package net.madvirus.spring4.chap16.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,11 +13,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-				.withUser("admin").password("1234").roles("ADMIN").and()
-				.withUser("madvirus").password("abcd").and()
-				.withUser("bkchoi").password("asdf");
+			.withUser("bkchoi").password("1234").roles("MEMBER").and()
+			.withUser("admin").password("asdf").roles("ADMIN", "MEMBER");
 	}
-	
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	}
 
 }

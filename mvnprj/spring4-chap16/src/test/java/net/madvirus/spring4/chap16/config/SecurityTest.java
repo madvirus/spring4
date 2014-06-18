1791970cf2849e7081f120c6da17a5a6d677bbca
madvirus <madvirus@madvirus.net>
2014-06-18 19:00:50 +0900

    불필요 의존 모듈 제거, 불필요 코드 제거, 로그아웃 테스트 추가

--- mvnprj/spring4-chap16/src/test/java/net/madvirus/spring4/chap16/config/SecurityTest.java
+++ mvnprj/spring4-chap16/src/test/java/net/madvirus/spring4/chap16/config/SecurityTest.java
@@ -35,36 +35,64 @@ public class SecurityTest {
 
 	private MockMvc mockMvc;
 
-	private MockHttpSession mockSession;
-
 	@Before
 	public void init() {
 		DelegatingFilterProxy securityFilter = new DelegatingFilterProxy();
 		securityFilter.setTargetBeanName("springSecurityFilterChain");
 		securityFilter.setServletContext(context.getServletContext());
 		mockMvc = MockMvcBuilders.webAppContextSetup(context).addFilter(securityFilter, "/*").build();
-		mockSession = new MockHttpSession();
 	}
 
 	@Test
 	public void test_Successfully_Login_Redirect_Process_When_AnonymousUser_Access_To_Restricted_Resource()
 			throws Exception {
-		mockMvc.perform(get("/admin/main").session(mockSession))
+		MvcResult result = mockMvc.perform(get("/admin/main"))
 				.andDo(print())
 				.andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrl(HTTP_LOCALHOST + "/spring_security_login"));
+				.andExpect(redirectedUrl(HTTP_LOCALHOST + "/spring_security_login"))
+				.andReturn();
 
-		dumpSession(mockSession);
+		HttpSession session = result.getRequest().getSession(false);
+		dumpSession(session);
 
-		MvcResult result = mockMvc.perform(
+		result = mockMvc.perform(
 				post("/j_spring_security_check").param("j_username", "admin").param("j_password", "asdf")
-						.session(mockSession))
+						.session(toMock(session)))
 				.andDo(print())
 				.andExpect(status().is3xxRedirection())
 				.andExpect(redirectedUrl(HTTP_LOCALHOST + "/admin/main"))
 				.andReturn();
 
 		dumpSession(result.getRequest().getSession(false));
+	}
+	
+	@Test
+	public void test_Logout() throws Exception {
+		MvcResult result = mockMvc.perform(get("/j_spring_security_logout"))
+				.andDo(print())
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/"))
+				.andReturn();
+	}
+	
+
+	private MockHttpSession toMock(HttpSession session) {
+		if (session instanceof MockHttpSession)
+			return (MockHttpSession) session;
+		else
+			return createMockHttpSessionFromSession(session);
+	}
+
+	private MockHttpSession createMockHttpSessionFromSession(HttpSession session) {
+		if (session == null)
+			return null;
+		MockHttpSession mockSession = new MockHttpSession();
+		Enumeration<String> attrNames = session.getAttributeNames();
+		while (attrNames.hasMoreElements()) {
+			String name = attrNames.nextElement();
+			mockSession.setAttribute(name, session.getAttribute(name));
+		}
+		return mockSession;
 	}
 
 	private void dumpSession(HttpSession session) {

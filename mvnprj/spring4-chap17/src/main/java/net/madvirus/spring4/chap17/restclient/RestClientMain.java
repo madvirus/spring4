7b1c4a17a2b2c5293621e9fc5d69d5d2044c6b6c
madvirus <madvirus@madvirus.net>
2014-06-25 08:32:19 +0900

    get

--- mvnprj/spring4-chap17/src/main/java/net/madvirus/spring4/chap17/restclient/RestClientMain.java
+++ mvnprj/spring4-chap17/src/main/java/net/madvirus/spring4/chap17/restclient/RestClientMain.java
@@ -1,11 +1,15 @@
 package net.madvirus.spring4.chap17.restclient;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class RestClientMain {
 
 	public static void main(String[] args) {
 		RestTemplate restTemplate = new RestTemplate();
-//		restTemplate.get
+		ResponseEntity<String> responseEntity = 
+				restTemplate.getForEntity("http://www.daum.net", String.class);
+		String body = responseEntity.getBody();
+		System.out.println(body);
 	}
 }

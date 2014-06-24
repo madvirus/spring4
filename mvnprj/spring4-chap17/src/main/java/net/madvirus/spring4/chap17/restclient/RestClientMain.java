33808a75265a0c123d6b0bfcba35b796ed32917e
madvirus <madvirus@madvirus.net>
2014-06-24 18:16:14 +0900

    REST 템플릿 의존 추가

---
+++ mvnprj/spring4-chap17/src/main/java/net/madvirus/spring4/chap17/restclient/RestClientMain.java
@@ -0,0 +1,11 @@
+package net.madvirus.spring4.chap17.restclient;
+
+import org.springframework.web.client.RestTemplate;
+
+public class RestClientMain {
+
+	public static void main(String[] args) {
+		RestTemplate restTemplate = new RestTemplate();
+//		restTemplate.get
+	}
+}
